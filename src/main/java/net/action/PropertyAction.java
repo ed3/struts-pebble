9be2bd54ef0f5ff2ab511631c1c6a9a7fed0aaf1
Ed <ed3@users.noreply.github.com>
2026-05-08 11:55:40 +0300

    Add files via upload

--- src/main/java/net/action/PropertyAction.java
+++ src/main/java/net/action/PropertyAction.java
@@ -1,18 +1,31 @@
 package net.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.struts2.ActionSupport;
 
 public class PropertyAction extends ActionSupport {
-	private static final long serialVersionUID = 1L;
-	private String name = "Property!";
-	private String[] names = {"A","B"};
-	public String execute() throws Exception {
-		return SUCCESS;
-	}
-	public String getName() {
-		return name;
-	}
-	public String[] getNames() {
-		return names;
-	}
+    private static final long serialVersionUID = 1L;
+    private String pro = "Property!";
+    private String[] letters = {"A", "B"};
+    private List<String> names = new ArrayList<String>();
+
+    public String execute() throws Exception {
+        names.add("Ed");
+        names.add("Lee");
+        return SUCCESS;
+    }
+
+    public String getPro() {
+        return pro;
+    }
+
+    public String[] getLetters() {
+        return letters;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
 }
